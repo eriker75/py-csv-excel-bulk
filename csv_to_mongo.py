6917174882783580643a0b6e823934aec907a339
eriker75 <eribertmarquez75@gmail.com>
2023-11-08 13:38:11 -0400

    new files

---
+++ csv_to_mongo.py
@@ -0,0 +1,17 @@
+import csv
+from pymongo import MongoClient
+
+
+def csv_to_mongo():
+    print("csv to mongo")
+    return ''
+
+
+"""
+mongo_client = MongoClient()
+db = mongo_client["db"]
+collection = db["collection"]
+
+with open('big_in_csv.csv') as file:
+    csv.reader(file)
+"""
--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+from csv_to_mongo import csv_to_mongo
+
+
 def main():
+    csv_to_mongo()
     print("Hello World")
 
 
